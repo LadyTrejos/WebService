b0d563457e3b7f4a62d825df1ec5284150da5220
MauroCG <mauro57.mauricio@gmail.com>
2019-05-20 21:48:28 -0500

    Se integró ocn bokeh para la visualización de los datos. Solo se ha puesto una gráfica de prueba por el momento.

--- WebServices/apps/home/views.py
+++ WebServices/apps/home/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
 from sodapy import Socrata
 import pandas as pd
+from bokeh.plotting import figure
+from bokeh.embed import components
 
 def home(request):
+	# Obtaining the dataset
 	client = Socrata('www.datos.gov.co', 'k25EB69oNekTcnbXvrgklJRQn')
 	capturas = client.get('qa6k-wzms', limit=1000)
+	
+	# Creating the data frame and the json for the boostrap table
 	data_frame = pd.DataFrame(capturas)
 	json = data_frame.to_json(orient='records')
 
-	return render(request, 'home/index.html', {'data_frame': data_frame, 'json': json})
+	# Example of bokeh figure
+	x = [1, 2, 3, 4, 5]
+	y = [1, 2, 3, 4, 5]
+
+	plot = figure(title='Line Graph', x_axis_label='X', y_axis_label='Y', plot_width=400, plot_height=400)
+	plot.line(x, y, line_width=2)
+
+	script, div = components(plot)
+
+	return render(request, 'home/index.html', 
+		{
+		'data_frame': data_frame, 
+		'json': json, 
+		'bokehScript': script,
+		'bokehDiv': div
+		})
